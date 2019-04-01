454b2591eede9430d77072c1e1e00c9f5dd6e381
Roberto E. B. Junior <robertoeb.jr@gmail.com>
2019-04-01 14:49:04 -0300

    Challenge 21

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-(function() {
-    var name = 'Roberto Eustáquio';
-    console.log( name );
-}) ();
+(function(win, doc) {
+    'use strict'
+
+    var counter = 0;
+    var $button = doc.querySelector('[data-js="button"]');
+    var temporizador;
+
+    function timer() {
+        console.log( 'timer:', counter++ );
+        temporizador = setTimeout( timer, 1000 );
+    }
+
+    timer();
+
+    $button.addEventListener('click', function() {
+        clearTimeout(temporizador);
+    }, false)
+}) (window, document);
